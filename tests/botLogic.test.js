356eef63a79faa65b49bfaf817fbe9b126be3cd0
Bastien Rouviere <bastien_r@orange.fr>
2025-07-31 10:10:47 +0200

    modification du test : ajout d'un mock du gameState

--- tests/botLogic.test.js
+++ tests/botLogic.test.js
@@ -1,7 +1,14 @@
 import { decideMove } from '../src/botLogic.js';
 
 test('returns valid move and action', () => {
-  const result = decideMove({});
+  const mockGameState = {
+    you: { id: 'test-bot', x: 1, y: 1 },
+    points: [{ x: 3, y: 1 }],
+    megaPoint: { x: 5, y: 5 },
+  };
+
+  const result = decideMove(mockGameState);
+
   expect(result).toHaveProperty('move');
   expect(result).toHaveProperty('action');
 });
